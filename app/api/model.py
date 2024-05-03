d990df7a5ca6ecbb3320adc10a32f6795b8cad98
joscaz <joscazertuche@gmail.com>
2024-05-03 10:22:26 -0600

    Added table codigo for file uploading

--- app/api/model.py
+++ app/api/model.py
@@ -11,13 +11,14 @@ class Prueba(Base):
     fecha: Mapped[DateTime] = mapped_column(DateTime, default=db.func.current_timestamp())
     estado: Mapped[bool] = mapped_column(Boolean, nullable=False)
     cambio_aceptado: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    id_codigo: Mapped[int] = mapped_column(Integer, ForeignKey('codigo.id_codigo'), nullable=True)
 
 class Version(Base):
     __tablename__ = 'version'
     
     id_version: Mapped[int] = mapped_column(Integer, primary_key=True)
     numero_version: Mapped[str] = mapped_column(String(50), nullable=False)
-    github_url: Mapped[str] = mapped_column(String(200), nullable=False)
+    github_url: Mapped[str] = mapped_column(String(200), nullable=True)
 
 class Reporte(Base):
     __tablename__ = 'reporte'
@@ -26,6 +27,7 @@ class Reporte(Base):
     fecha: Mapped[DateTime] = mapped_column(DateTime, default=db.func.current_timestamp())
     contenido: Mapped[str] = mapped_column(Text, nullable=False)
     id_prueba: Mapped[int] = mapped_column(Integer, ForeignKey('prueba.id_prueba'))
+    id_codigo: Mapped[int] = mapped_column(Integer, ForeignKey('codigo.id_codigo'))  # Asegúrate de que este campo esté definido
 
 class Elemento(Base):
     __tablename__ = 'elemento'
@@ -35,3 +37,11 @@ class Elemento(Base):
     localizador: Mapped[str] = mapped_column(String(200), nullable=False)
     estado: Mapped[bool] = mapped_column(Boolean, nullable=False)
     id_prueba: Mapped[int] = mapped_column(Integer, ForeignKey('prueba.id_prueba'))
+
+class Codigo(Base):
+    __tablename__ = 'codigo'
+
+    id_codigo: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nombre_archivo: Mapped[str] = mapped_column(String(100), nullable=False)
+    contenido: Mapped[str] = mapped_column(Text, nullable=False)
+    fecha_subida: Mapped[DateTime] = mapped_column(DateTime, default=db.func.current_timestamp())
