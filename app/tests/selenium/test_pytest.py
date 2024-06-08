a48d712ad6fbad61a6c337ef0a27b06a0526643b
joscaz <joscazertuche@gmail.com>
2024-06-07 21:27:21 -0600

    Fixes

--- app/tests/selenium/test_pytest.py
+++ app/tests/selenium/test_pytest.py
@@ -11,13 +11,13 @@ from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
-# options.add_argument("window-size=1400,1500")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("start-maximized")
-# options.add_argument("enable-automation")
-# options.add_argument("--disable-infobars")
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
 @pytest.fixture(scope='session')
 def driver():
